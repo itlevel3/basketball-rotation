63534756cb22399212abab4e49201bd1d5ade873
Amit Nir <amitnir@Amits-MacBook-Pro-M1.local>
2024-11-30 20:21:40 -0500

    fixing build

--- components/ui/alert.jsx
+++ components/ui/alert.jsx
@@ -1,15 +1,11 @@
 import * as React from "react"
-import { cva } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const Alert = React.forwardRef(({ className, variant, ...props }, ref) => (
   <div
     ref={ref}
     role="alert"
-    className={cn(
-      "relative w-full rounded-lg border p-4",
-      className
-    )}
+    className={cn("relative w-full rounded-lg border p-4", className)}
     {...props} />
 ))
 Alert.displayName = "Alert"
